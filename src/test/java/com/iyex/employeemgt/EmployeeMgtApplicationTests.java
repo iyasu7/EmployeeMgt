eee60ca26aa7d33a229b7e30d7cb05fc1f741031
iyasu7 <107022433+iyasu7@users.noreply.github.com>
2023-06-08 15:07:10 +0300

    Initial commit

---
+++ src/test/java/com/iyex/employeemgt/EmployeeMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iyex.employeemgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMgtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
